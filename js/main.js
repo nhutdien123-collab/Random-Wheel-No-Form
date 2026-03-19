63d1b2b112ac5b2f5b31a0b286a1d478bf246ad1
nhutdien123-collab <nhutdien123@gmail.com>
2026-03-19 11:27:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var prizes = [
     { text: "Quạt ITC", img: "images/quat.png", percentpage: 0.15 },
     { text: "Chúc bạn may mắn lần sau", img: "images/hengaplai.png", percentpage: 0.25 },
     { text: "Túi vải", img: "images/hengaplai.png", percentpage: 0.03 },
-    { text: "Gấu bông", img: "images/gaubong.png", percentpage: 0.02 },
+    { text: "Gấu bông", img: "images/gaubong01.png", percentpage: 0.02 },
     { text: "Check in nhận quà", img: "images/check in.png", percentpage: 0.03 },
     { text: "Balo ITC", img: "images/balo.png", percentpage: 0.02 },
     { text: "Túi vải", img: "images/hengaplai.png", percentpage: 0.03 },
